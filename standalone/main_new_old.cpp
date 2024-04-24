5000a97e5a7178f55c07d5c58df7f9563c468c8a
Edu Meneses <eduardo@edumeneses.com>
2024-04-24 16:57:29 -0400

    fix ossia handling

--- standalone/main_new_old.cpp
+++ standalone/main_new_old.cpp
@@ -33,31 +33,53 @@ ossia::net::generic_device device {
         device_name
 };
 
-puara_gestures::Shake3D shake;
-puara_gestures::Jab3D jab;
+puara_gestures::Shake shake;
+puara_gestures::Jab jab;
+
 
 auto cb = [](ossia::string_view unhandled_node, const ossia::value& unhandled_value) {
     std::cout 
-        << "Received unhandled message (" 
+        << "Received new message. Creating node and associated gestures (" 
         << unhandled_node << " => " 
         << ossia::value_to_pretty_string(unhandled_value) <<  ")."
         << std::endl; 
-    };
+    if (unhandled_value.get_type() != ossia::val_type::INT && unhandled_value.get_type() != ossia::val_type::FLOAT) {
+        std::cout << "Not creating node. Wrong value type (expecting single float or integer)" << std::endl;
+        return;
+    }
+    auto& node = create_node(device, unhandled_node);
+    auto param = node.create_parameter(unhandled_value.get_type());
+    switch (unhandled_value.get_type()) {
+        case ossia::val_type::INT:
+            // for now do nothing but need to add button stuff here
+            break;
+        case ossia::val_type::FLOAT:
+            shake.emplace(unhandled_node, puara_gestures::Shake());
+            jab.emplace(unhandled_node, puara_gestures::Jab());
+            param->add_callback([=](const ossia::value& received_value) {
+                std::cout << "New value received: " << ossia::value_to_pretty_string(received_value) << std::endl;
+                std::cout << "index: " << param->get_node().get_name() << std::endl;
+                // std::cout << "shake: " << 
+                //     shake.at(param->get_node().get_name()).update(ossia::convert<float>(received_value))
+                //     << std::endl;
+            });
+            break;
+    }
+};
 
 int main(int argc, char* argv[]) {
 
     // creating response for unhandled messages
     device.on_unhandled_message.connect(&cb);
 
-    // creating accelerometer 3D node
-    auto& accelNode = find_or_create_node(device, "/puaragestures/accel3D");
+    // creating nodes and parameters
+    auto& accelNode = find_or_create_node(device, "/puaragestures/accel");
     auto accelParam = accelNode.create_parameter(ossia::val_type::LIST);
-    accelParam->add_callback([&](const ossia::value& v) {
-        std::cout << "New accelerometer value received: " << ossia::value_to_pretty_string(v) << std::endl;
-        ossia::vec3f accelerometer = ossia::convert<ossia::vec3f>(v);
-        shake.update(accelerometer[0], accelerometer[1], accelerometer[2]);
-        jab.update(accelerometer[0], accelerometer[1], accelerometer[2]);
-    });
+
+    // param->add_callback([&](const ossia::value& v) {
+    //     std::cout << "New value received: " << ossia::value_to_pretty_string(v) << std::endl;
+    //     i = ossia::convert<int>(v); 
+    // });
 
     while(true) {
 
