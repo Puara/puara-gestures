45b62abd4b4a46af1b50c8370871e8cfa12e9f57
mneedham01 <mneedham@smith.edu>
2024-08-07 11:54:20 -0400

    read in new testing files

--- testing_roll.h
+++ testing_roll.h
@@ -41,60 +41,59 @@ class testing_roll {
         std::list<std::string> selected = {"roll_clock.txt", "roll_counter.txt", "stat_down.txt", "stat_left.txt", "stat_right.txt", "stat_up.txt" };
         std::list<std::string> onefile = {"roll_clock.txt"};
 
-        for (std::string s : onefile) {
+        // for (std::string s : onefile) {
 
-            std::cout << "\n----------------------------------------------------------------------\n";
-            std::cout << s << "\n";
-            std::cout << "----------------------------------------------------------------------\n";
+        //     std::cout << "\n----------------------------------------------------------------------\n";
+        //     std::cout << s << "\n";
+        //     std::cout << "----------------------------------------------------------------------\n";
 
             // set up common path
             std::string common = "../../test_files/t-stick/";
 
             // read in accl data
-            std::string accl_path = common + "accl/" + s;
-            std::cout << "accl_path = " << accl_path << "\n";
+            std::string accl_path = common + "accel_raw.csv";
             std::ifstream accl_file(accl_path);
             std::string accl_line;
 
             // read in gyro data
-            std::string gyro_path = common + "gyro/" + s;
+            std::string gyro_path = common + "gyro_raw.csv";;
             std::ifstream gyro_file(gyro_path);
             std::string gyro_line;
 
             // read in mag data
-            std::string mag_path = common +  "mag/" + s;
+            std::string mag_path = common +  "mag_raw.csv";
             std::ifstream mag_file(mag_path);
             std::string mag_line;
 
-            // read in ypr data
-            std::string ypr_path = common +  "ypr/" + s;
-            std::ifstream ypr_file(ypr_path);
-            std::string ypr_line;
+            // read in roll data
+            std::string roll_path = common +  "roll_raw.csv";
+            std::ifstream roll_file(roll_path);
+            std::string roll_line;
 
             // utils::TimeDifference timeDiff;
-            while (std::getline(accl_file, accl_line, '\n') && std::getline(gyro_file, gyro_line, '\n') && std::getline(mag_file, mag_line, '\n') && std::getline(ypr_file, ypr_line, '\n')) {
-                if ( accl_line.empty() || gyro_line.empty() || mag_line.empty() || ypr_line.empty() ) {
+            while (std::getline(accl_file, accl_line, '\n') && std::getline(gyro_file, gyro_line, '\n') && std::getline(mag_file, mag_line, '\n') && std::getline(roll_file, roll_line, '\n')) {
+                if (accl_line.empty() || gyro_line.empty() || mag_line.empty() || roll_line.empty() ) {
                     break;
                 }
 
                 // get and set coordinates in orientation object
                 // get accl coordinate
                 puara_gestures::Coord3D accl;
-                accl = utils::readinRaw(accl_line);
+                accl = utils::readinRawCSV(accl_line);
                 // accl.x = accl.x / 9.80665;
                 // accl.y = accl.y / 9.80665;
                 // accl.z = accl.z / 9.80665;
 
                 // get gyro coordinates
                 puara_gestures::Coord3D gyro;
-                gyro = utils::readinRaw(gyro_line);
+                gyro = utils::readinRawCSV(gyro_line);
                 // gyro.x =  gyro.x * 180 / M_PI;
                 // gyro.y =  gyro.y * 180 / M_PI;
                 // gyro.z =  gyro.z * 180 / M_PI;
 
                 // get mag coordinates
                 puara_gestures::Coord3D mag;
-                mag = utils::readinRaw(mag_line);
+                mag = utils::readinRawCSV(mag_line);
 
                 // calibrate magnetometer
 
@@ -108,20 +107,20 @@ class testing_roll {
                 // mag.y = sy[0]*(mag.y-h[1]) + sy[1]*(mag.y-h[1]) + sy[2]*(mag.y-h[1]);
                 // mag.z = sz[0]*(mag.z-h[2]) + sz[1]*(mag.z-h[2]) + sz[2]*(mag.z-h[2]);
 
-                // get yaw, pitch, and roll
-                puara_gestures::Coord3D ypr;
-                ypr = utils::readinRaw(ypr_line);
+                // get roll
+                double roll;
+                roll = utils::readinRawRoll(roll_line);
 
                 long then = utils::getCurrentTimeMicroseconds();
                 long now = utils::getCurrentTimeMicroseconds();
-                std::cout << "Puara = " << test.update(accl, gyro, mag, ((then - now))* 0.000001) << "; ";
-                std::cout << "Max = " << ypr.z << "\n";
+                std::cout << "Puara Function = " << test.update(accl, gyro, mag, ((then - now))* 0.000001) << "; ";
+                std::cout << "Roll value = " << roll << "\n";
                 // std::cout << "; unwrapped = " << test.unwrap(ypr.z);
                 // std::cout << "; wrapped = " << test.wrap(test.unwrap(ypr.z), 0, 6.28) << "\n";
             }
             test.clear_unwrap();
             test.clear_smooth();
-        }
+        //}
     };
 };
 };
