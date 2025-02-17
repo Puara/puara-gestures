d900ea3e9af545a3c4a2765efa8b0eb1a7be2301
Vincent Berthiaume <vberthiaume@gmail.com>
2025-02-17 14:22:18 -0500

    cleanup

--- Builds/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/barth/Documents/git/sat/puara/puara-gestures
-# Build directory: C:/Users/barth/Documents/git/sat/puara/puara-gestures/Builds
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
