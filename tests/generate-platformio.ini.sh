f753d6f8b581de47ede58add3cc98a8d9dcf2ba8
Charles B <bicari.charles@gmail.com>
2026-03-30 16:32:59 -0400

    templated the platformio.ini file to add current hash of active commit

---
+++ tests/generate-platformio.ini.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -eu
+
+# Variables
+PUARA_GESTURES_COMMIT_HASH=$1
+
+if [[ -z "$PUARA_GESTURES_PATH" ]]; then
+  PUARA_GESTURES_PATH="https://github.com/Puara/puara-gestures.git$PUARA_GESTURES_COMMIT_HASH"
+fi
+
+# We'll put the temporary platformio.ini file in tests/platformio/
+OUTPUT_FILE="tests/platformio/platformio.ini"
+
+# Write the file
+cat <<EOL > "${OUTPUT_FILE}"
+[platformio]
+description = Embedded Compilation Tests for Puara Gestures Library
+default_envs = build_with_3rdparty_libs
+[env:build_with_3rdparty_libs]
+platform = https://github.com/pioarduino/platform-espressif32/releases/download/stable/platform-espressif32.zip
+board = tinypico
+framework = arduino
+build_flags =
+    -I../include
+    -I../3rdparty
+    -I../3rdparty/eigen
+    -I../3rdparty/IMU_Sensor_Fusion
+lib_deps = $PUARA_GESTURES_PATH
+EOL
