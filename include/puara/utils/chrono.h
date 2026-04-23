97831cae56ffbc56891f049e804a832e503da314
Charles B <bicari.charles@gmail.com>
2026-04-23 11:02:47 -0400

    time f() changed to reduce crossplatform issues where some systems may implement past f() with system_clock and return negative time measures which can be problematic

--- include/puara/utils/chrono.h
+++ include/puara/utils/chrono.h
@@ -19,7 +19,8 @@ namespace puara_gestures::utils
  *  @brief Simple function to get the current elapsed time in microseconds.
  *
  *  This helper is intended for portable use in both desktop and embedded
- *  test code that needs a monotonic timestamp.
+ *  test code that needs a monotonic timestamp. It returns the time since an
+ *  arbitrary epoch and is not intended to represent "wall-clock" time.
  *
  *  Example:
  *    auto start = getCurrentTimeMicroseconds();
@@ -31,7 +32,7 @@ namespace puara_gestures::utils
  */
 inline unsigned long long getCurrentTimeMicroseconds()
 {
-  auto currentTimePoint = std::chrono::high_resolution_clock::now();
+  auto currentTimePoint = std::chrono::steady_clock::now();
   auto duration = std::chrono::duration_cast<std::chrono::microseconds>(
       currentTimePoint.time_since_epoch());
   return duration.count();
