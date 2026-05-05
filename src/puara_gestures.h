00d5bb5a6a8eb64062b4fe70fdaf6b0cc3f920a5
Charles B <bicari.charles@gmail.com>
2026-05-05 14:44:07 -0400

    added header for arduino linting and CI compatibility

---
+++ src/puara_gestures.h
@@ -0,0 +1,7 @@
+// This header is used to include the main gestures header in the Arduino library,
+// and to avoid issues with the Arduino IDE not supporting certain C++ features. 
+// It should be included in the main Arduino library header (puara_gestures.h) and
+// should not be included directly in user code.
+
+#pragma once
+#include <puara/gestures.h>
