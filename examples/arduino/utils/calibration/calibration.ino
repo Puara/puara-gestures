bc1270b5e7680d2d6824507e3ec9d298a197a0b6
Charles B <bicari.charles@gmail.com>
2026-05-27 14:15:32 -0400

    adding appropriate header declaration in calibration example

--- examples/arduino/utils/calibration/calibration.ino
+++ examples/arduino/utils/calibration/calibration.ino
@@ -52,6 +52,7 @@
 
 #include <Arduino.h>
 #include <ArduinoEigen.h>
+#include <boost-embedded-190.h>
 #include <puara-gestures.h>
 
 // ---------------------------------------------------------------------------
